054c0d9872357d552d2979bb171f84e3d01b8df7
Nox Freebird <Nox@niks-mbp.mynet>
2017-03-17 13:30:09 +0200

    made a button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TextButton from './TextButton';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-          {[1,2,3].map(i=>(
+      <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
+      </div>
+      {[1,2,3].map(i=>(
         <p className="App-intro">
-          {i}. To get started, edit <code>src/App.js</code> and save to reload.
+        {i}. To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        ))}
+      ))}
+        <TextButton/>
       </div>
     );
   }
