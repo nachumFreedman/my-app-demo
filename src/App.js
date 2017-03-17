3cb1413ef7476e0ef43baf49db56f64bd70cca53
Nox Freebird <Nox@niks-mbp.mynet>
2017-03-17 13:32:37 +0200

    indentation fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         {i}. To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       ))}
-        <TextButton/>
+      <TextButton/>
       </div>
     );
   }
--- src/TextButton.js
+++ src/TextButton.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 class TextButton extends Component {
-render(){
-return(
-<div >
-<p>You should</p>
-<button>"Click me"</button>
-</div>
+  render(){
+    return(
+      <div >
+      <p>You should</p>
+      <button>"Click me"</button>
+      </div>
 
 
-)};
-};
-export default TextButton;
+    )};
+  };
+  export default TextButton;
